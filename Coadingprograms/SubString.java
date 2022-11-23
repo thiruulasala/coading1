20ca1775af0832ac8cb1139bc996f8f916023022
thiruulasala <96606591+thiruulasala@users.noreply.github.com>
2022-11-23 20:27:52 +0530

    update substring java

--- Coadingprograms/SubString.java
+++ Coadingprograms/SubString.java
@@ -4,7 +4,7 @@ public class SubString
 {
     public static void main(String[] args)
     {
-        String str=new String("Hello world welcome to earth");
+        String str=new String("Hello  world welcome to earth");
         System.out.print("te extracted substring is  :  ");
         System.out.println(str.substring(10));
         System.out.println(str.substring(05,14));
